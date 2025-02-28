be7512775ff7939bfba8005751c823da2edb422c
HamidRaza1012 <hamidsaeed1012@gmail.com>
2025-02-28 19:04:12 +0500

    Javascript

---
+++ app.js
@@ -0,0 +1,176 @@
+//Assigment1
+
+//Question 1
+
+//alert("Hey Welcome!")
+
+//Question 2
+
+//alert("Error! Please  enter a valid password.")
+ 
+//Question 3
+
+//alert("Welcom to JS Land..\n Happy coding!")
+
+//Question 4
+
+//alert("Welcom to JS land")
+//alert("Happy coding!")
+
+//Question 5
+
+//console.log("Hello I can run js through my webs browser")
+
+
+
+//-----------Assigment#2--------------
+
+//Question 1
+
+//var username
+
+//var myname="Hamid Raza"
+
+//Question 3
+
+//var message="Hello World"
+//alert(message)
+
+//Question 4
+
+//var name="Hamid Raza"
+//alert(name)
+//var age="Im 18 Years old"
+//alert(age)
+//var course="Web development"
+//alert(course)
+
+//Question 6
+
+//var email="hamisaeed1012@gmail.com"
+//alert("My email address is"+" " +email)
+
+//Question 7
+
+//var book="A smarter \n Way to learn javascript";
+//alert("Im trying to learn from the book"+ " "+book)
+
+
+//Question 8
+
+//document.write("Yah! I can write HTML content through JS");
+
+//Question 9
+
+//var message = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+
+//alert(message);
+
+//document.write(message);
+
+
+//---------------- -VARIABLE FOR NUMBERS------------
+//-----------------ASSIGMENT 3---------------------
+
+//Question 1
+
+//var age= 15;
+//alert("I am"+ " "+age+" "+"years old")
+
+
+//Question 3
+
+//var birthyear= 2007;
+//document.write("My birth year is"+" "+birthyear+"<br>");
+//document.write("Data type of my declared variable is"+" "+typeof birthyear);
+
+//question  4
+
+
+//var visitorname="john Doe";
+//var producttiltle="T Shirts";
+//var quantity=5;
+
+//document.write(visitorname+" "+"Orderd"+" "+quantity+" "+producttiltle+" "+"on xyz clothing store");
+
+
+//---------------------ASSIGMENT#4---------------------
+//--------------------VARAIABLE NAMES:ILLEGAL AND LEGAL-----------------------
+
+//                              VARIABLE NAMES: LEGAL & ILLEGAL 
+
+//                                    QUESTION 1
+
+//var variable1, variable2, variable3;
+
+//                                    QUESTION 2
+//                                      LEGAL 
+// userName
+// _age
+// $price
+// product123
+// myVariable
+
+//                                      ILLEGAL 
+// 123abc
+// user-name
+// var
+// my variable
+// @username
+
+//                                    QUESTION 3
+
+//document.write("<h1>Rules for naming JS variables</h1>");
+
+//document.write("<p>Variable names can only contain letters, numbers, dollar signs ($), and underscores (_). For example: <strong>$my_1stVariable</strong></p>");
+
+//document.write("<p>Variables must begin with a letter, dollar sign ($), or underscore (_). For example: <strong>$name</strong>, <strong>_name</strong>, or <strong>name</strong>.</p>");
+
+//document.write("<p>Variable names are case sensitive.</p>");
+
+//document.write("<p>Variable names should not be JavaScript reserved keywords.</p>");
+
+
+
+                                    // MATH EXPRESSIONS
+// QUESTION 01
+
+//var a = 3;
+//var b = 5;
+
+//console.log(a + b);
+//document.write("Sum of " + a + " and " + b + " is " + (a + b));
+
+// QUESTION 02
+
+// SUBTRACT
+
+//var a = 3;
+//var b = 5;
+
+//console.log(a - b);
+//document.write("subtraction of " + a + " and " + b + " is " + (a - b));
+
+// MULTIPLY
+
+//var a = 3;
+//var b = 5;
+
+//console.log(a * b);
+//document.write("multiply of " + a + " and " + b + " is " + (a * b));
+
+// DIVIDE
+
+//var a = 3;
+//var b = 5;
+
+//console.log(a / b);
+//document.write("division of " + a + " and " + b + " is " + (a / b));
+
+// MODULUS
+
+//var a = 3;
+//var b = 5;
+
+//console.log(a % b);
+//document.write("modulus of " + a + " and " + b + " is " + (a % b));
