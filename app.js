4c88b26145adb774de32400f476eb036b7bd29a9
HamidRaza1012 <hamidsaeed1012@gmail.com>
2025-03-02 15:00:22 +0500

    Javascript

--- app.js
+++ app.js
@@ -174,3 +174,189 @@
 
 //console.log(a % b);
 //document.write("modulus of " + a + " and " + b + " is " + (a % b));
+
+
+
+// // QUESTION 03
+
+// // a. Declare a variable
+// var num;
+
+// // b. Show the value of variable in your browser
+// document.write("Value after variable declaration is: " + num + "<br>");
+
+// // c. Initialize the variable with some number
+// num = 5;
+
+// // d. Show the value of variable
+// document.write("Initial value: " + num + "<br>");
+
+// // e. Increment the variable
+// num++;
+
+// // f. Show the value of variable after increment
+// document.write("Value after increment is: " + num + "<br>");
+
+// // g. Add 7 to the variable
+// num += 7;
+
+// // h. Show the value after addition
+// document.write("Value after addition is: " + num + "<br>");
+
+// // i. Decrement the variable
+// num--;
+
+// // j. Show the value of variable after decrement
+// document.write("Value after decrement is: " + num + "<br>");
+
+// // k. Show the remainder after dividing the variable’s value by 3
+// var remainder = num % 3;
+
+// // l.remainder
+// document.write("The remainder is: " + remainder + "<br>");
+
+// // QUESTION 04
+
+// // Store the cost of one movie ticket
+// var ticketPrice = 600;
+
+// // Calculate the cost of buying 5 tickets
+// var totalCost = ticketPrice * 5;
+
+// // Display the result
+// document.write("The cost of buying 5 movie tickets is " + totalCost + " PKR");
+
+
+
+// // QUESTION 06
+
+//  // a. Store a Celsius temperature into a variable
+//  var celsius = 25;
+
+//  // b. Convert it to Fahrenheit and output "NNoC is NNoF"
+//  var fahrenheit = (celsius * 9/5) + 32;
+//  document.write(celsius + "°C = " + fahrenheit + "°F<br>");
+
+//  // c. Now store a Fahrenheit temperature into a variable
+//  var fahrenheitTemp = 77;
+
+//  // d. Convert it to Celsius and output "NNoF is NNoC"
+//  var celsiusTemp = (fahrenheitTemp - 32) * 5/9;
+//  document.write(fahrenheitTemp + "°F = " + celsiusTemp.toFixed(2) + "°C");
+
+// // QUESTION 07
+
+// // a. Price of item 1
+// var item1Price = 600;  // Price of item 1 in PKR
+
+// // b. Price of item 2
+// var item2Price = 100;  // Price of item 2 in PKR
+
+// // c. Ordered quantity of item 1
+// var item1Quantity = 3;  // Ordered quantity of item 1
+
+// // d. Ordered quantity of item 2
+// var item2Quantity = 7;  // Ordered quantity of item 2
+
+// // e. Shipping charges
+// var shippingCharges = 50;  // Shipping charges in PKR
+
+// // Calculate total cost of each item
+// var totalItem1Cost = item1Price * item1Quantity;
+// var totalItem2Cost = item2Price * item2Quantity;
+
+// // Calculate total cost of the order
+// var totalCost = totalItem1Cost + totalItem2Cost + shippingCharges;
+
+// // Display the receipt in the browser
+// document.write("<h2>Shopping Cart</h2>");
+// document.write("Price of Item 1 is " + item1Price + " <br>");
+// document.write("Quantity of Item 1 is " + item1Quantity + "<br>");
+// document.write("Price of Item 2 is " + item2Price + " <br>");
+// document.write("Quantity of Item 2 is " + item2Quantity + "<br>");
+// document.write("Shipping Charges is " + shippingCharges + " <br> <hr>");
+// document.write("Total Cost of your order is " + totalCost + "<br>");
+
+// // QUESTION 08
+
+// var totalMarks = 980;
+// var marksObtained = 804;
+
+// // Display total marks and marks obtained
+// document.write("Total Marks: " + totalMarks + "<br>");
+// document.write("Marks Obtained: " + marksObtained + "<br>");
+
+// // Calculate and display the percentage
+// var percentage = (marksObtained / totalMarks) * 100;
+// document.write("Percentage: " + percentage.toFixed(2) + "% <br>");
+
+// // QUESTION 09
+
+// // Given exchange rates
+// const usdToPkr = 104.80;
+// const sarToPkr = 28;
+
+// // Total amount in USD and SAR
+// const usd = 10;
+// const sar = 25;
+
+// // Conversion in a single expression
+// const totalPkr = (usd * usdToPkr) + (sar * sarToPkr);
+
+// // Output the result
+// document.write("Total:" + totalPkr + " PKR <br>");
+
+// // QUESSTION 10
+
+// var num = 10; // Initialize with any number
+// var result = ((num + 5) * 10) / 2;
+// document.write("Answer:" + result + "<br>");
+
+// // QUESTION 11
+
+// var currentYear = 2025; // Store the current year
+// var birthYear = 2004; // Store the birth year
+
+// var age1 = currentYear - birthYear;
+// var age2 = age1 - 1; // The person might not have had their birthday yet
+
+// document.write("<h1>Age Calculator</h1>")
+// document.write("They are either " + age2 + " or " + age1 + " years old. <br>");
+
+// // QUESTION 12
+
+// // Store the radius
+// var radius = 20;
+
+// // Approximate value of π
+// var pi = 3.142; 
+
+//  // Formula: 2πr
+// var circumference = 2 * pi * radius;
+
+//  // Formula: πr²
+// var area = pi * radius * radius;
+
+// document.write("<h1>The Geometrizer</h1>");
+// document.write("Radius of Circle is " + radius + "<br>");
+// document.write("The circumference is " + circumference + "<br>");
+// document.write("The area is " + area + "<br>");
+
+// // QUESTION 13
+
+//  // Store favorite snack
+// var favoriteSnack = "Lays";
+
+//  // Store current age
+// var currentAge = 20;
+
+//  // Store maximum expected age
+// var maxAge = 80;
+
+//  // Store estimated amount per day
+// var amountPerDay = 30;
+
+//  // Calculate total supply
+// var totalNeeded = (maxAge - currentAge) * 365 * amountPerDay;
+
+// document.write("You will need " + totalNeeded + " " + favoriteSnack + " to last you until the ripe old age of " + maxAge + ".");
